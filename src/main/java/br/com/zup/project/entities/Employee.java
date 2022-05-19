fbe80b8b6c6f80b5ecab24d5b144c16926ba21a1
lucasmm56 <lucasmm56@gmail.com>
2022-05-19 20:46:33 -0300

    Inserção da classe Employee

---
+++ src/main/java/br/com/zup/project/entities/Employee.java
@@ -0,0 +1,44 @@
+package br.com.zup.project.entities;
+
+import java.util.Objects;
+
+public class Employee {
+
+	private int id;
+	private String name;
+	private String email;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return id == other.id;
+	}
+
+}
