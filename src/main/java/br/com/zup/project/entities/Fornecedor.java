d50a831162391355476d8c7329ebc917deed6922
1BrenoOliveira <1Brenomiranda@gmail.com>
2022-05-23 21:50:26 -0300

    Ad To_String

--- src/main/java/br/com/zup/project/entities/Fornecedor.java
+++ src/main/java/br/com/zup/project/entities/Fornecedor.java
@@ -16,5 +16,11 @@ public class Fornecedor {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
+    @Override
+    public String toString() {
+        return "Fornecedor{" + "id=" + id + ", nome=" + nome + '}';
+    }
+        
 	
 }
