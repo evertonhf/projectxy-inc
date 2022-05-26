4c8d807a855ef92e6a38ab9150f482353294e048
Eleodorio <netinhoeleodorio@gmail.com>
2022-05-26 20:30:48 -0300

    Classe bike lele

---
+++ src/main/java/br/com/zup/project/entities/Bike.java
@@ -0,0 +1,27 @@
+package br.com.zup.project.entities;
+
+public class Bike {
+private int id;
+private String marca;
+private String modelo;
+
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getMarca() {
+	return marca;
+}
+public void setMarca(String marca) {
+	this.marca = marca;
+}
+public String getModelo() {
+	return modelo;
+}
+public void setModelo(String modelo) {
+	this.modelo = modelo;
+}
+
+}
