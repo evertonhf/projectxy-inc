d291c01a396df21aa009971915e5596182a6212f
Eleodorio <netinhoeleodorio@gmail.com>
2022-05-26 20:40:49 -0300

    Atualização para Usuario

---
+++ src/main/java/br/com/zup/project/entities/Usuario.java
@@ -0,0 +1,27 @@
+package br.com.zup.project.entities;
+
+public class Usuario {
+private int id;
+private String Nome;
+private String email;
+
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getNome() {
+	return Nome;
+}
+public void setNome(String Nome) {
+	this.Nome = Nome;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+
+}
